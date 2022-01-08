cb1e6a22c6fed7d9f8b3dcf9c5f0645580c5f299
jurica29 <jrcpuljiz@gmail.com>
2022-01-08 19:14:00 +0000

    Add code to check answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {   //gameType variable declared in the loop down there !!! let gameType 
     let buttons = document.getElementsByTagName("button"); //display button when the page is loaded
 
     for (let button of buttons) {                                    //using "this" to refer to a specific button, clicking submit refers to it, get method checks content
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");     //if its submit it will display this message
+                  checkAnswer(); //this function will check answer (it is defined lower in the code)
             } else {
-                let gameType = this.getAttribute("data-type");     //otherwise we set up the value of game type to the value of the attribute and this will tell us what game type we want to run
+            let gameType = this.getAttribute("data-type");     //otherwise we set up the value of game type to the value of the attribute and this will tell us what game type we want to run
                 runGame(gameType);                 //that happens in case we click on any other buttons on the page except from submit button
             }
         });
@@ -36,11 +36,42 @@ function runGame(gameType) {
 
 }
 
+/**
+ * Checks the answer agaist the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+  let userAnswer = parseInt(document.getElementbyId("answer-box").value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
+  
+  if (isCorrect) {
+    alert("Hey! You got it right! :D");
+} else {
+    alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+}
+
+runGame(calculatedAnswer[1]);
 
 }
 
+/**
+* Gets the operands (the numbers) and the operator (plus, minus etc)
+* directly from the dom, and returns the correct answer.
+*/
 function calculateCorrectAnswer() {
+
+  let operand1 = parseInt(document.getElementById('operand1').innerText); //parseInt treats it as an integer
+  let operand2 = parseInt(document.getElementById('operand2').innerText);
+  let operator = (document.getElementById("operator").innerText);
+
+  if (operator === "+") {                                          //if its a plus sign it will calculate the correct answer, but if its not it will return error message
+      return [operand1 + operand2, "addition"];
+  } else {
+      alert (`Unimplemented operator ${operator}`);
+      throw `Unimplemented operator ${operator}. Aborting!`);
+  }
+  }
 
 }
 
